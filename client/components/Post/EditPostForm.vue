f4969d20b3a5c552ba06953de0b05deaf5969c39
Ben Sheres <bsheres@mit.edu>
2024-10-17 23:40:32 -0400

    Alpha 5 I think that fixed it

--- client/components/Post/EditPostForm.vue
+++ client/components/Post/EditPostForm.vue
@@ -26,7 +26,6 @@ const editPost = async (content: string) => {
       <menu>
         <li><button class="btn-small pure-button-primary pure-button" type="submit">Save</button></li>
         <li><button class="btn-small pure-button" @click="emit('editPost')">Cancel</button></li>
-        <SearchPostForm @getPostsByAuthor="getPosts" />
       </menu>
       <p v-if="props.post.dateCreated !== props.post.dateUpdated" class="timestamp">Edited on: {{ formatDate(props.post.dateUpdated) }}</p>
       <p v-else class="timestamp">Created on: {{ formatDate(props.post.dateCreated) }}</p>
